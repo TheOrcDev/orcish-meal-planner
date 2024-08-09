a26b4fa0a6cef72ffedfa5797cdf99a2c310c6ed
papaurke <urosmiric88@gmail.com>
2024-08-09 23:51:22 +0200

    refactor(create-meal-plan-form): make ai prompt more optimized
    prompt

--- components/features/create-meal-plan-form/prompt.ts
+++ components/features/create-meal-plan-form/prompt.ts
@@ -4,35 +4,37 @@ import { formSchema } from "./create-meal-plan-form";
 
 export const getPrompt = (values: z.infer<typeof formSchema>): string => {
   let prompt = `
-    Give me a meal plan for the entire day.
+    Create a customized meal plan for the entire day based on the following details:
 
-    My age: ${values.age}
-    My sex: ${values.sex}
-
-    I want to have ${values.meals} meals a day.
-
-    My diet should be: ${values.diet}
-
-    My main goal is: ${values.goal}
+    - Age: ${values.age}
+    - Sex: ${values.sex}
+    - Number of meals per day: ${values.meals}
+    - Diet type: ${values.diet} (e.g., vegetarian, keto, low-carb)
+    - Primary goal: ${values.goal} (e.g., weight loss, muscle gain, maintenance)
     `;
 
   if (values.allergies) {
     prompt = `
       ${prompt}
-      
-      These are my allergies, and things I don't like: ${values.allergies}
+      - Allergies and dislikes: ${values.allergies}
       `;
   }
 
   const jsonFormat = `
+    Please ensure that the meal plan aligns with my nutritional needs and goals, 
+    offering a balanced and varied diet. Include options for breakfast, 
+    lunch, dinner, and snacks, and consider any specific dietary preferences or 
+    restrictions. Provide detailed meal ideas with portion sizes, 
+    ingredient suggestions, and nutritional information if possible.
+
+    Ingredients should have amount of food in grams like this: Chicken Breasts (200g)
+
     The result should be returned in the following format, and please keep it in one line, nothing should break the JSON formatting:
 
     { 
       meals: { mealTitle: string, calories: string, ingredients: string[] }[]
       totalCalories: string
     }
-
-    Ingredients should have amount of food in grams like this: Chicken Breasts (200g)
     `;
 
   return `${prompt} ${jsonFormat}`;
