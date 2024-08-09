61b47ef22926a608da6c640de4d50220f2affbfe
papaurke <urosmiric88@gmail.com>
2024-08-09 22:23:13 +0200

    feat(user-info): add dark mode for user button
    clerk

--- components/ui/user-info/user-info.tsx
+++ components/ui/user-info/user-info.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import Link from "next/link";
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useTheme } from "next-themes";
+import { dark } from "@clerk/themes";
 
 import { trpc } from "@/server/client";
 import { Badge, Button } from "@/components/ui";
-import Link from "next/link";
 
 export default function UserInfo() {
   const tokens = trpc.tokens.getTokens.useQuery();
+  const { resolvedTheme } = useTheme();
 
   return (
     <div className="flex items-center gap-2">
@@ -22,7 +26,11 @@ export default function UserInfo() {
           <Badge>{tokens?.data} tokens</Badge>
         </Link>
 
-        <UserButton />
+        <UserButton
+          appearance={{
+            baseTheme: resolvedTheme === "dark" ? dark : undefined,
+          }}
+        />
       </SignedIn>
     </div>
   );
