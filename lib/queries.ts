d2802d484ec6c9cbe4f40ee9f14ab8e3d0b39d64
papaurke <urosmiric88@gmail.com>
2024-08-09 22:16:37 +0200

    feat(lib): add free tokens
    free tokens for each new user

--- lib/queries.ts
+++ lib/queries.ts
@@ -2,6 +2,8 @@ import db from "@/db/drizzle";
 import { purchases, tokenSpends } from "@/db/schema";
 import { count, eq, sum } from "drizzle-orm";
 
+const FREE_TOKENS = 5;
+
 export const getTotalTokens = async (email: string): Promise<number> => {
     try {
         const [tokens] = await db.
@@ -10,14 +12,14 @@ export const getTotalTokens = async (email: string): Promise<number> => {
             where(eq(purchases.email, email));
 
         if (!tokens.value) {
-            return 0;
+            tokens.value = "0"
         }
 
         const [tokensSpend] = await db.select({ count: count() }).
             from(tokenSpends).
             where(eq(tokenSpends.email, email));
 
-        return +tokens.value - tokensSpend.count;
+        return +tokens.value - tokensSpend.count + FREE_TOKENS;
     } catch (e) {
         throw (e);
     }
