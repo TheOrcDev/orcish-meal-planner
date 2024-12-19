681cf0c7e5e8f01389195aa9966b8a1acafc3f38
OrcDev <urosmiric88@gmail.com>
2024-12-19 17:13:54 +0100

    feat(header) add h1

--- components/ui/header/header.tsx
+++ components/ui/header/header.tsx
@@ -6,7 +6,7 @@ import UserInfo from "../user-info/user-info";
 
 export default function Header() {
   return (
-    <header className="absolute z-10 flex w-full items-center justify-between p-5">
+    <header className="absolute z-10 flex w-full items-center justify-between px-5 py-2">
       <Link href={"/"} className="flex items-center gap-3">
         <Image
           width={50}
@@ -15,6 +15,7 @@ export default function Header() {
           alt="Meal Planner Logo"
           priority
         />
+        <h1 className="hidden text-2xl font-bold md:block">Meal Planner</h1>
       </Link>
 
       <div className="flex gap-3">
