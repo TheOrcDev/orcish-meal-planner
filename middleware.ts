053334b26a8e9e9cca55145590971934d53f1137
OrcDev <urosmiric88@gmail.com>
2024-12-19 13:08:08 +0100

    fix(middleware) add await for clerk

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,14 @@
-import {
-    clerkMiddleware,
-    createRouteMatcher
-} from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-    '/meal-planner(.*)',
-    '/meal-plans(.*)',
+  "/meal-planner(.*)",
+  "/meal-plans(.*)",
 ]);
 
-export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) {
-        auth().protect();
-    }
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
 });
 
 export const config = {
-    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
