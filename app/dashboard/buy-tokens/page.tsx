b7c9c46cf2b93cdad2bcab144e438c0c6f87aad4
OrcDev <urosmiric88@gmail.com>
2024-12-22 00:08:02 +0100

    fix(buy-tokens) remove header

--- app/dashboard/buy-tokens/page.tsx
+++ app/dashboard/buy-tokens/page.tsx
@@ -1,13 +1,9 @@
 import { BuyTokens } from "@/components/features";
-import { Header } from "@/components/ui";
 
 export default async function BuyTokensPage() {
   return (
-    <>
-      <Header />
-      <div className="flex flex-col items-center justify-center gap-10 p-24">
-        <BuyTokens />
-      </div>
-    </>
+    <div className="flex flex-col items-center justify-center gap-10">
+      <BuyTokens />
+    </div>
   );
 }
