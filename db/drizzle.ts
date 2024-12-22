9d0ef058076b869039cebc2eebb7294896c181ed
OrcDev <urosmiric88@gmail.com>
2024-12-22 21:53:28 +0100

    fix(drizzle) arrange imports

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,6 +1,7 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from './schema';
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+import * as schema from "./schema";
 
 const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle(sql, { schema });
