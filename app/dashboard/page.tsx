9b0d63fe0529e8f6d6be59e77103a23070ec13a8
OrcDev <urosmiric88@gmail.com>
2025-01-03 22:44:44 +0100

    feat(dashboard) add coming soon text and image

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+
 export default function Page() {
   return (
-    <main className="flex flex-col items-center justify-center p-4 md:p-8">
+    <main className="flex flex-col items-center justify-center gap-5 p-4 md:p-8">
       <h1 className="text-2xl font-bold">Dashboard</h1>
+      <h3>Awesome dashboard coming soon</h3>
+      <Image src="/meal-planner.png" alt="Dashboard" width={500} height={500} />
     </main>
   );
 }
