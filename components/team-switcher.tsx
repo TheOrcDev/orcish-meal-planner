673663196995a650f687cb76557d57ee67f69c33
OrcDev <urosmiric88@gmail.com>
2025-01-04 17:44:46 +0100

    feat(team-switcher) remove background color from logo

--- components/team-switcher.tsx
+++ components/team-switcher.tsx
@@ -17,13 +17,12 @@ export function TeamSwitcher() {
             size="lg"
             className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
           >
-            <div className="flex aspect-square size-10 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+            <div className="flex aspect-square size-10 items-center justify-center">
               <Image
                 width={70}
                 height={70}
                 src={"/meal-planner.png"}
                 alt="Meal Planner Logo"
-                priority
               />
             </div>
             <div className="grid flex-1 text-left text-sm leading-tight">
