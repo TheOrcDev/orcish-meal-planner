0d79d36b5f6d5534d763ec4a9c52dd026a94f3f0
OrcDev <urosmiric88@gmail.com>
2025-01-04 11:56:21 +0100

    feat(team-switcher) add link to the dashboard

--- components/team-switcher.tsx
+++ components/team-switcher.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import * as React from "react";
 
 import {
@@ -11,26 +12,28 @@ export function TeamSwitcher() {
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <SidebarMenuButton
-          size="lg"
-          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-        >
-          <div className="flex aspect-square size-10 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-            <Image
-              width={70}
-              height={70}
-              src={"/meal-planner.png"}
-              alt="Meal Planner Logo"
-              priority
-            />
-          </div>
-          <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="truncate font-semibold">Meal Planner</span>
-            <span className="truncate text-xs">
-              Transform your eating habits
-            </span>
-          </div>
-        </SidebarMenuButton>
+        <Link href="/dashboard">
+          <SidebarMenuButton
+            size="lg"
+            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+          >
+            <div className="flex aspect-square size-10 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+              <Image
+                width={70}
+                height={70}
+                src={"/meal-planner.png"}
+                alt="Meal Planner Logo"
+                priority
+              />
+            </div>
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <span className="truncate font-semibold">Meal Planner</span>
+              <span className="truncate text-xs">
+                Transform your eating habits
+              </span>
+            </div>
+          </SidebarMenuButton>
+        </Link>
       </SidebarMenuItem>
     </SidebarMenu>
   );
