9ea336678a74eb0c67ac2e652e8d9fd6e70290f1
OrcDev <urosmiric88@gmail.com>
2025-03-12 16:24:43 +0100

    refactor(dashboard) remove tokens

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,11 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { getTokens } from "@/server/tokens";
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const tokens = await getTokens();
-
   return (
     <SidebarProvider>
       <AppSidebar />
